832cae94de407d1394dd9bcc4b643df6124b7e36
mayurdoshi5 <mayur@aivhub.com>
2023-04-30 21:51:59 +0100

    MD: updated file

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { SpreadsheetAllModule } from '@syncfusion/ej2-angular-spreadsheet';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SpreadsheetAllModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
